126d97b0e3b507c053d2c217688c71bf1556e43e
gireesh <gireeshnaidu2921996@gmail.com>
2022-12-14 10:03:24 +0000

    calculate ratio

---
+++ utilities/ratioFactorial/index.js
@@ -0,0 +1,12 @@
+/*const factorialOfNumber = require("../utilities/factorial/index");
+const ratioOfTwoNumbers = require("../utilities/ratio.index");
+
+module.exports = factorialOfNumber;
+module.exports = ratioOfTwoNumbers;*/
+
+const ratioAndFactorial = (num1, num2, num3) => {
+  const ratio = ratioOfTwoNumbers(num1, num2);
+  const factorial = factorialOfNumber(num);
+
+  return ratio, factorial;
+};
